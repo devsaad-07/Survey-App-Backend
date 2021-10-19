51071a43752b91c44a8e4f8828271fc6a2f0258a
Dev Saad <saad.dev07@gmail.com>
2021-10-20 00:41:47 +0530

    fix cors

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 const pingRouter = require("./routes/ping");
 app.use("/", pingRouter);
