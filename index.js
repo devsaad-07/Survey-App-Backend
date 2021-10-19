ec2fe6e5ce9b51c5b62415e4bea1a9b42077afa5
Dev Saad <saad.dev07@gmail.com>
2021-10-19 16:14:04 +0530

    Setup api

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 
-const homeRouter = require("./routes/home");
-app.use("/", homeRouter);
 const pingRouter = require("./routes/ping");
 app.use("/", pingRouter);
 
--- routes/ping.js
+++ routes/ping.js
@@ -3,11 +3,51 @@ const db = require("../configs/firebaseconfig");
 
 const router = express.Router();
 
-router.get("/ping", (req, res) => {
-  db.ref("/sampleData/" + "alanIsAwesome").set({
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing",
+// function stringToHash(string) {
+//   var hash = 0;
+
+//   if (string.length == 0) return hash;
+
+//   for (i = 0; i < string.length; i++) {
+//     char = string.charCodeAt(i);
+//     hash = (hash << 5) - hash + char;
+//     hash = hash & hash;
+//   }
+
+//   return hash;
+// }
+
+router.post("/sign-up", (req, res) => {
+  const user = req.body;
+  db.ref(`/Survey-App/Users`).push(user);
+  res.send(user);
+});
+
+router.post("/create_survey", (req, res) => {
+  const survey = req.body;
+  const dbRef = db.ref(`/Survey-App/Surveys`).push({
+    Questions: survey,
   });
+  res.send(survey);
+});
+
+router.get("/get_survey/:surveyID", (req, res) => {
+  db.ref(`/Survey-App/Surveys/${req.params.surveyID}/Questions`).once(
+    "value",
+    function (snapshot) {
+      snapshot.forEach(function (child) {
+        console.log(child.key + ": " + child.val());
+      });
+    }
+  );
+  res.send("pong");
+});
+
+router.post("/submit_survey/:surveyID/:userID", (req, res) => {
+  const answers = req.body;
+  db.ref(
+    `/Survey-App/Surveys/${req.params.surveyID}/Answers/${req.params.userID}`
+  ).set(answers);
   res.send("pong");
 });
 
