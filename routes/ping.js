4d6210bcf4ee9ca1b92181898d11563ec1ebc3cf
Dev Saad <saad.dev07@gmail.com>
2021-10-19 16:52:21 +0530

    Fix API

--- routes/ping.js
+++ routes/ping.js
@@ -3,20 +3,9 @@ const db = require("../configs/firebaseconfig");
 
 const router = express.Router();
 
-// function stringToHash(string) {
-//   var hash = 0;
-
-//   if (string.length == 0) return hash;
-
-//   for (i = 0; i < string.length; i++) {
-//     char = string.charCodeAt(i);
-//     hash = (hash << 5) - hash + char;
-//     hash = hash & hash;
-//   }
-
-//   return hash;
-// }
-
+router.get("/", (req, res) => {
+  res.send("Hi");
+});
 router.post("/sign-up", (req, res) => {
   const user = req.body;
   db.ref(`/Survey-App/Users`).push(user);
@@ -25,22 +14,25 @@ router.post("/sign-up", (req, res) => {
 
 router.post("/create_survey", (req, res) => {
   const survey = req.body;
-  const dbRef = db.ref(`/Survey-App/Surveys`).push({
+  db.ref(`/Survey-App/Surveys`).push({
     Questions: survey,
   });
   res.send(survey);
 });
 
 router.get("/get_survey/:surveyID", (req, res) => {
+  const survey = {};
   db.ref(`/Survey-App/Surveys/${req.params.surveyID}/Questions`).once(
     "value",
     function (snapshot) {
       snapshot.forEach(function (child) {
-        console.log(child.key + ": " + child.val());
+        survey[child.key] = child.val();
+        // console.log(`${child.key}: ${child.val()}`);
+        // console.log(survey);
       });
+      res.send(survey);
     }
   );
-  res.send("pong");
 });
 
 router.post("/submit_survey/:surveyID/:userID", (req, res) => {
@@ -48,7 +40,7 @@ router.post("/submit_survey/:surveyID/:userID", (req, res) => {
   db.ref(
     `/Survey-App/Surveys/${req.params.surveyID}/Answers/${req.params.userID}`
   ).set(answers);
-  res.send("pong");
+  res.send("Answers Saved!");
 });
 
 module.exports = router;
